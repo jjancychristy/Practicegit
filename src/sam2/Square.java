595697e3a03d8ba4b6ccfb453e08ea72a3cb25da
jjancychristy <jjancychristy24081988@gmail.com>
2023-01-04 16:14:43 -0500

    second commit

---
+++ src/sam2/Square.java
@@ -0,0 +1,22 @@
+package sam2;
+
+public class Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int length=5;
+  int breath=10;
+  
+  if(length==breath)
+  {
+	  System.out.println("It is a square");
+  }
+  else {
+	  
+	  System.out.println("It is a rectangle");
+	    }
+  
+	  
+	}
+
+}

20792f2e723cdaf65335e9bd8c50ccc053031d11
jjancychristy <jjancychristy24081988@gmail.com>
2023-01-04 16:25:35 -0500

    third

---
+++ src/sam2/Assignment.java
@@ -0,0 +1,20 @@
+package sam2;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// calculate the sum of first 10 natural number.//
+		
+		int sum=0;
+		int num=10;
+		for(int i=0;i<num;i++)
+		{
+			sum=sum+i;
+		}
+			System.out.println(sum);
+		
+		
+	}
+
+}

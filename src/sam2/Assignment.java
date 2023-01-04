80ef8b126227e3090695c64587ee2d1306d8fe0f
jjancychristy <jjancychristy24081988@gmail.com>
2023-01-04 16:33:43 -0500

    fourth

--- src/sam2/Assignment.java
+++ src/sam2/Assignment.java
@@ -4,9 +4,10 @@ public class Assignment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-// calculate the sum of first 10 natural number.//
+// calculate the sum of first 10 natural number//
 		
 		int sum=0;
+		
 		int num=10;
 		for(int i=0;i<num;i++)
 		{
